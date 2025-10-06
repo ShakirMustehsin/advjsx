b77c13a349d85860c802c544776b794914b5b6f1
Astracalce <shakirmustehsin@gmail.com>
2025-10-06 13:42:07 +0530

    trying

--- Inception/app.js
+++ Inception/app.js
@@ -1,4 +1,23 @@
- const heading = React.createElement("h1", {}, "this is hello from react");
-        const root = ReactDOM.createRoot(document.querySelector(".root"));
+/**
+ * div #parent 
+ *    div # child 
+ *         h1 
+ *      div # child 2 
+ *            h1 
+ *            h2 
+ */
 
-        root.render(heading);
+const parent = React.createElement("div", {id : "parent"}, 
+      [ React.createElement("div", {id : "child"}, 
+              [React.createElement("h1", {}, " this is h1 form child 1"),
+                     React.createElement("h2", {}, " this is h2 form child 1")
+              ]
+       ), React.createElement("div", {id : "child2"}, 
+              [React.createElement("h1", {}, " this is h1 form child 2"),
+                     React.createElement("h2", {}, " this is h2 form child 2")
+              ]
+       )]
+)
+
+const root = ReactDOM.createRoot(document.querySelector(".root"));
+root.render (parent);
