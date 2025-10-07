3952a1b713b9fe868d32e0cbd5f8a312cf4b0613
Astracalce <shakirmustehsin@gmail.com>
2025-10-07 19:43:47 +0530

    final 3

--- Laying the foundation/app.js
+++ Laying the foundation/app.js
@@ -1,8 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// this is js codde 
+// this is js codde
 
-const jsxheading = <h1 className="jsxheading" tabIndex={5}> this is the h1 from jsx </h1>
+const JsxHeading =  (
+  <h1 className="jsxheading" tabIndex={5}>
+    this is the h1 from jsx
+  </h1>
+);
+
+const FrstComponent = () => (
+  <div>
+    {JsxHeading}
+    <h1>This is functional componnedt hahahah</h1>
+  </div>
+);
 const root = ReactDOM.createRoot(document.querySelector(".root"));
-root.render(jsxheading);
+root.render(<FrstComponent />);
