208db1546aee757562b9a4be9c87c8170444d618
Astracalce <shakirmustehsin@gmail.com>
2025-10-08 20:26:36 +0530

    jsxedited

--- Laying the foundation/app.js
+++ Laying the foundation/app.js
@@ -8,10 +8,11 @@ const JsxHeading =  (
     this is the h1 from jsx
   </h1>
 );
-
+const JsxHead = () =>  <h2>This is h2 from jsx</h2>;
 const FrstComponent = () => (
   <div>
     {JsxHeading}
+    {JsxHead}
     <h1>This is functional componnedt hahahah</h1>
   </div>
 );
