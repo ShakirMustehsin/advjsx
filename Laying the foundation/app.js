a30674ae7a608ace35ddfa4850f27403947f1dbb
Astracalce <shakirmustehsin@gmail.com>
2025-10-13 21:20:45 +0530

    sorted

--- Laying the foundation/app.js
+++ Laying the foundation/app.js
@@ -1,21 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import NavBar from "./components/NavBar";
 
 
-const NavBar = ()=>{
-  return (
-    <div className="navbar">
-           <div className="logo">
-                <img alt="Logo loaded" src="https://images.unsplash.com/photo-1612810806563-4cb8265db55f?q=80&w=327&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
-           </div>
-           <div className="nav-bar-links">
-              <h4>Home</h4>
-              <h4>Offers</h4>
-              <h4>About</h4>
-           </div>
-    </div>
-  )
-}
+
 
 const RestList =  [
                   {
