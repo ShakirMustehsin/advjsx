79cfb6e732452c2d90a2a987ab83ebc7d8a296d8
Astracalce <shakirmustehsin@gmail.com>
2025-10-17 19:23:56 +0530

    err hancling

--- Laying the foundation/app.js
+++ Laying the foundation/app.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import NavBar from "./components/NavBar";
 import Body from "./components/Body";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/error";
 
 const App = ()=> {
   return (
@@ -12,5 +16,25 @@ const App = ()=> {
     </div>
   )
 }
+
+const appRouters = createBrowserRouter([
+  {
+    "path" : "/",
+      "element" : <App></App>,
+      "errorElement" : <Error/>
+  },
+  {
+    "path" : "/about",
+    "element" : <About/>,
+    "errorElement" : <Error/>
+  },
+  {
+    "path" : "/contact",
+    "element" : <Contact/>,
+    "errorElement" : <Error/>
+  }
+])
+
+
 const root = ReactDOM.createRoot(document.querySelector(".root"));
-root.render(<App/>);
+root.render(<RouterProvider router = {appRouters}/>);
