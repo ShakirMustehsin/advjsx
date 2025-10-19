9a51d5613c6cd8dc8f6a8593ab2f2b160787b855
Astracalce <shakirmustehsin@gmail.com>
2025-10-19 21:07:20 +0530

    sks

--- Laying the foundation/app.js
+++ Laying the foundation/app.js
@@ -2,38 +2,41 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import NavBar from "./components/NavBar";
 import Body from "./components/Body";
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/error";
 
-const App = ()=> {
+const App = () => {
   return (
     <div>
-<NavBar/>
-<Body/>
+      <NavBar />
+      <Outlet />
     </div>
-  )
-}
+  );
+};
 
 const appRouters = createBrowserRouter([
   {
-    "path" : "/",
-      "element" : <App></App>,
-      "errorElement" : <Error/>
+    path: "/",
+    element: <App></App>,
+    children: [
+      {
+        path: "/",
+        element: <Body/>,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  errorElement: <Error />
   },
-  {
-    "path" : "/about",
-    "element" : <About/>,
-    "errorElement" : <Error/>
-  },
-  {
-    "path" : "/contact",
-    "element" : <Contact/>,
-    "errorElement" : <Error/>
-  }
-])
-
+]);
 
 const root = ReactDOM.createRoot(document.querySelector(".root"));
-root.render(<RouterProvider router = {appRouters}/>);
+root.render(<RouterProvider router={appRouters} />);
