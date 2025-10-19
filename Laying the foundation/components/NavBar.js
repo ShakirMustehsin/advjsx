9a48af7724a4aa0f40a7ed31342281f158cd6473
Astracalce <shakirmustehsin@gmail.com>
2025-10-19 22:11:11 +0530

    commit

--- Laying the foundation/components/NavBar.js
+++ Laying the foundation/components/NavBar.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { Link } from "react-router";
+
 
 const NavBar = ()=>{
 
@@ -9,9 +11,9 @@ const NavBar = ()=>{
                 <img alt="Logo loaded" src="https://images.unsplash.com/photo-1612810806563-4cb8265db55f?q=80&w=327&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
            </div>
            <div className="nav-bar-links">
-              <h4>Home</h4>
-              <h4>Offers</h4>
-              <h4>About</h4>
+              <h4> <Link to="/">Home</Link></h4>
+              <h4> <Link to="/contact">Offers</Link></h4>
+              <h4> <Link to="/about">About</Link></h4>
               <button onClick={()=> {
                       if (search === "login") setSearch("logout")
                         else setSearch("login");
