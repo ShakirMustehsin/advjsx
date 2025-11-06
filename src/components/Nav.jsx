bb3a672360afd5813b17416cd2e51fe1501b973c
Astracalce <shakirmustehsin@gmail.com>
2025-11-06 18:33:45 +0530

    s

---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+const Nav = () => {
+    return (
+        <div className="navbar">
+             
+             <h1>Logo</h1>
+             <div>
+                <h4>home</h4>
+                <h4>home</h4>
+                <h4>home</h4>
+                <h4>home</h4>
+             </div>
+
+        </div>
+    )
+}
+export default Nav;
